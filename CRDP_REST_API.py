775c7594b19bf078b89a3ee77ffa1e12218e7b8f
Rick Robinson <Rick.Robinson@ThalesGroup.com>
2024-12-11 11:17:26 -0500

    ruff formatting
    applied ruff to source code to clean up.

--- CRDP_REST_API.py
+++ CRDP_REST_API.py
@@ -7,159 +7,192 @@
 # API Ref: https://thalesdocs.com/ctp/con/crdp/latest/crdp-apis/index.html
 #
 ######################################################################
-import  requests
-from    urllib3.exceptions import InsecureRequestWarning
-import  json
+import requests
+import json
 
-import  enum
-import  re
 
 # ---------------- CONSTANTS -----------------------------------------------------
-STATUS_CODE_OK      = 200
+STATUS_CODE_OK = 200
 
-CRDP_PROTECT                        = "/v1/protect"
-CRDP_REVEAL                         = "/v1/reveal"
-CRDP_BULK_PROTECT                   = "/v1/protectbulk"
-CRDP_BULK_REVEAL                    = "/v1/revealbulk"
-CRDP_PROTECTION_POLICY_NAME         = "protection_policy_name"
-CRDP_DATA_NAME                      = "data"
-CRDP_DATA_ARRAY_NAME                = "data_array"
-CRDP_PROTECTED_DATA_NAME            = "protected_data"
-CRDP_PROTECTED_DATA_ARRAY_NAME      = "protected_data_array"
-CRDP_EXTERNAL_VER_NAME              = "external_version"
-CRDP_USERNAME_NAME                  = "username"
+CRDP_PROTECT = "/v1/protect"
+CRDP_REVEAL = "/v1/reveal"
+CRDP_BULK_PROTECT = "/v1/protectbulk"
+CRDP_BULK_REVEAL = "/v1/revealbulk"
+CRDP_PROTECTION_POLICY_NAME = "protection_policy_name"
+CRDP_DATA_NAME = "data"
+CRDP_DATA_ARRAY_NAME = "data_array"
+CRDP_PROTECTED_DATA_NAME = "protected_data"
+CRDP_PROTECTED_DATA_ARRAY_NAME = "protected_data_array"
+CRDP_EXTERNAL_VER_NAME = "external_version"
+CRDP_USERNAME_NAME = "username"
 
-APP_CONTENT_TYPE            = "Content-Type"
-APP_JSON                    = "application/json"
+APP_CONTENT_TYPE = "Content-Type"
+APP_JSON = "application/json"
+
 
 def protectData(t_hostCRDP, t_data, t_protectionPolicy):
-# -----------------------------------------------------------------------------
-# REST Assembly for data protection
-# 
-# Assemble and send the command to CRDP for protecting (encrypting) data and
-# retrieve the result and the external version.
-# -----------------------------------------------------------------------------
-    t_endpoint          = "http://%s%s" %(t_hostCRDP, CRDP_PROTECT)
+    # -----------------------------------------------------------------------------
+    # REST Assembly for data protection
+    #
+    # Assemble and send the command to CRDP for protecting (encrypting) data and
+    # retrieve the result and the external version.
+    # -----------------------------------------------------------------------------
+    t_endpoint = "http://%s%s" % (t_hostCRDP, CRDP_PROTECT)
 
-    t_headers           = {APP_CONTENT_TYPE:APP_JSON}
-    t_dataStr           = {CRDP_PROTECTION_POLICY_NAME:t_protectionPolicy, CRDP_DATA_NAME:t_data}
+    t_headers = {APP_CONTENT_TYPE: APP_JSON}
+    t_dataStr = {
+        CRDP_PROTECTION_POLICY_NAME: t_protectionPolicy,
+        CRDP_DATA_NAME: t_data,
+    }
 
     # Now that everything is populated, assemble and post command
-    r = requests.post(t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False)
+    r = requests.post(
+        t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False
+    )
 
-    if(r.status_code != STATUS_CODE_OK):
+    if r.status_code != STATUS_CODE_OK:
         kPrintError("protectData", r)
         exit()
 
     # Extract the UserAuthId from the value of the key-value pair of the JSON reponse.
-    t_protectedData       = r.json()[CRDP_PROTECTED_DATA_NAME]
-    t_version             = r.json()[CRDP_EXTERNAL_VER_NAME]  
-    
+    t_protectedData = r.json()[CRDP_PROTECTED_DATA_NAME]
+    t_version = r.json()[CRDP_EXTERNAL_VER_NAME]
+
     return t_protectedData, t_version
 
-def protectBulkData(t_hostCRDP, t_dataArray, t_protectionPolicy):
-# -----------------------------------------------------------------------------
-# REST Assembly for bulk data protection
-# 
-# Assemble and send the command to CRDP for protecting (encrypting) data and
-# retrieve the result and the external version as an array.
-# -----------------------------------------------------------------------------
-    t_endpoint          = "http://%s%s" %(t_hostCRDP, CRDP_BULK_PROTECT)
 
-    t_headers           = {APP_CONTENT_TYPE:APP_JSON}
-    t_dataStr           = {CRDP_PROTECTION_POLICY_NAME:t_protectionPolicy, CRDP_DATA_ARRAY_NAME:t_dataArray}
+def protectBulkData(t_hostCRDP, t_dataArray, t_protectionPolicy):
+    # -----------------------------------------------------------------------------
+    # REST Assembly for bulk data protection
+    #
+    # Assemble and send the command to CRDP for protecting (encrypting) data and
+    # retrieve the result and the external version as an array.
+    # -----------------------------------------------------------------------------
+    t_endpoint = "http://%s%s" % (t_hostCRDP, CRDP_BULK_PROTECT)
+
+    t_headers = {APP_CONTENT_TYPE: APP_JSON}
+    t_dataStr = {
+        CRDP_PROTECTION_POLICY_NAME: t_protectionPolicy,
+        CRDP_DATA_ARRAY_NAME: t_dataArray,
+    }
 
     # Now that everything is populated, assemble and post command
-    r = requests.post(t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False)
+    r = requests.post(
+        t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False
+    )
 
-    if(r.status_code != STATUS_CODE_OK):
+    if r.status_code != STATUS_CODE_OK:
         kPrintError("protectBulkData", r)
         exit()
 
     # Extract the UserAuthId from the value of the key-value pair of the JSON reponse.
-    t_protectedData       = r.json()[CRDP_PROTECTED_DATA_ARRAY_NAME]
-    t_version             = r.json()[CRDP_PROTECTED_DATA_ARRAY_NAME][0][CRDP_EXTERNAL_VER_NAME]  
-    
+    t_protectedData = r.json()[CRDP_PROTECTED_DATA_ARRAY_NAME]
+    t_version = r.json()[CRDP_PROTECTED_DATA_ARRAY_NAME][0][CRDP_EXTERNAL_VER_NAME]
+
     return t_protectedData, t_version
 
-def revealData(t_hostCRDP, t_data, t_protectionPolicy, t_externalVersion, t_user):
-# -----------------------------------------------------------------------------
-# REST Assembly for data reveal
-# 
-# Assemble and send the command to CRDP for reveal (decrypting) data and
-# retrieve the result and the external version.
-# -----------------------------------------------------------------------------
-    t_endpoint          = "http://%s%s" %(t_hostCRDP, CRDP_REVEAL)
 
-    t_headers           = {APP_CONTENT_TYPE:APP_JSON}
-    t_dataStr           = {CRDP_PROTECTION_POLICY_NAME:t_protectionPolicy, CRDP_EXTERNAL_VER_NAME:t_externalVersion, CRDP_USERNAME_NAME:t_user, CRDP_PROTECTED_DATA_NAME:t_data}
+def revealData(t_hostCRDP, t_data, t_protectionPolicy, t_externalVersion, t_user):
+    # -----------------------------------------------------------------------------
+    # REST Assembly for data reveal
+    #
+    # Assemble and send the command to CRDP for reveal (decrypting) data and
+    # retrieve the result and the external version.
+    # -----------------------------------------------------------------------------
+    t_endpoint = "http://%s%s" % (t_hostCRDP, CRDP_REVEAL)
+
+    t_headers = {APP_CONTENT_TYPE: APP_JSON}
+    t_dataStr = {
+        CRDP_PROTECTION_POLICY_NAME: t_protectionPolicy,
+        CRDP_EXTERNAL_VER_NAME: t_externalVersion,
+        CRDP_USERNAME_NAME: t_user,
+        CRDP_PROTECTED_DATA_NAME: t_data,
+    }
 
     # Now that everything is populated, assemble and post command
-    r = requests.post(t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False)
+    r = requests.post(
+        t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False
+    )
 
-    if(r.status_code != STATUS_CODE_OK):
+    if r.status_code != STATUS_CODE_OK:
         kPrintError("revealData", r)
         exit()
 
     # Extract the UserAuthId from the value of the key-value pair of the JSON reponse.
-    t_revealedData       = r.json()[CRDP_DATA_NAME]
-        
+    t_revealedData = r.json()[CRDP_DATA_NAME]
+
     return t_revealedData
 
-def revealBulkData(t_hostCRDP, t_dataArray, t_protectionPolicy, t_externalVersion, t_user):
-# -----------------------------------------------------------------------------
-# REST Assembly for bulk data reveal
-# 
-# Assemble and send the command to CRDP for prevealingg (decrypting) bulk data and
-# retrieve the result as an array.
-# -----------------------------------------------------------------------------
-    t_endpoint          = "http://%s%s" %(t_hostCRDP, CRDP_BULK_REVEAL)
 
-    t_headers           = {APP_CONTENT_TYPE:APP_JSON}
-    t_dataStr           = {CRDP_PROTECTION_POLICY_NAME:t_protectionPolicy, CRDP_EXTERNAL_VER_NAME:t_externalVersion, CRDP_USERNAME_NAME:t_user, CRDP_PROTECTED_DATA_ARRAY_NAME:t_dataArray}
+def revealBulkData(
+    t_hostCRDP, t_dataArray, t_protectionPolicy, t_externalVersion, t_user
+):
+    # -----------------------------------------------------------------------------
+    # REST Assembly for bulk data reveal
+    #
+    # Assemble and send the command to CRDP for prevealingg (decrypting) bulk data and
+    # retrieve the result as an array.
+    # -----------------------------------------------------------------------------
+    t_endpoint = "http://%s%s" % (t_hostCRDP, CRDP_BULK_REVEAL)
+
+    t_headers = {APP_CONTENT_TYPE: APP_JSON}
+    t_dataStr = {
+        CRDP_PROTECTION_POLICY_NAME: t_protectionPolicy,
+        CRDP_EXTERNAL_VER_NAME: t_externalVersion,
+        CRDP_USERNAME_NAME: t_user,
+        CRDP_PROTECTED_DATA_ARRAY_NAME: t_dataArray,
+    }
 
     # Now that everything is populated, assemble and post command
-    r = requests.post(t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False)
+    r = requests.post(
+        t_endpoint, data=json.dumps(t_dataStr), headers=t_headers, verify=False
+    )
 
-    if(r.status_code != STATUS_CODE_OK):
+    if r.status_code != STATUS_CODE_OK:
         kPrintError("revealBulkData", r)
         exit()
 
     # Extract the UserAuthId from the value of the key-value pair of the JSON reponse.
     t_revealedDataArray = r.json()[CRDP_DATA_ARRAY_NAME]
-     
+
     return t_revealedDataArray
 
+
 def kPrintError(t_str, t_r):
-# -----------------------------------------------------------------------------
-# The objective is to print the error information back in the even that a HTTPS
-# response is not STATUS_OK
-# -----------------------------------------------------------------------------
-    t_str_sc    = str(t_r.status_code)
-    t_str_r     = str(t_r.reason)
+    # -----------------------------------------------------------------------------
+    # The objective is to print the error information back in the even that a HTTPS
+    # response is not STATUS_OK
+    # -----------------------------------------------------------------------------
+    t_str_sc = str(t_r.status_code)
+    t_str_r = str(t_r.reason)
     # t_str_e     = str(t_r.json()['error'])
-    t_str_e     = str(t_r.json())
-    
-    tmpstr      = "  --> %s Status Code: %s\n   Reason: %s\n   Error: %s" %(t_str, t_str_sc, t_str_r, t_str_e)
-    
+    t_str_e = str(t_r.json())
+
+    tmpstr = "  --> %s Status Code: %s\n   Reason: %s\n   Error: %s" % (
+        t_str,
+        t_str_sc,
+        t_str_r,
+        t_str_e,
+    )
+
     print(tmpstr)
 
     return
 
+
 def makeHexStr(t_val):
-# -------------------------------------------------------------------------------
-# makeHexString
-# -------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------
+    # makeHexString
+    # -------------------------------------------------------------------------------
     tmpStr = str(t_val)
     t_hexStr = hex(int("0x" + tmpStr[2:-1], 0))
 
     return t_hexStr
 
-def printJList(t_str, t_jList):
-# -------------------------------------------------------------------------------
-# A quick subscript that makes it easy to print out a list of JSON information in
-# a more readable format.
-# -------------------------------------------------------------------------------    
-    print("\n ", t_str, json.dumps(t_jList, skipkeys = True, allow_nan = True, indent = 3))
 
-    
+def printJList(t_str, t_jList):
+    # -------------------------------------------------------------------------------
+    # A quick subscript that makes it easy to print out a list of JSON information in
+    # a more readable format.
+    # -------------------------------------------------------------------------------
+    print("\n ", t_str, json.dumps(t_jList, skipkeys=True, allow_nan=True, indent=3))
--- CRDP_Stress.py
+++ CRDP_Stress.py
@@ -6,90 +6,98 @@
 #
 # Time will be measured for the file to be encrypted excluding file I/O actions.
 #
-# Usage:  CRDP_Stress.py 
+# Usage:  CRDP_Stress.py
 #           -e <CRDP endpoint hostname or IP address>
 #           -i <input csv file>
 #           -b <batch size>
 #           -o <output filename>
 #           -h - header flag.  indicates if header is present in input csv file
 #
-import sys
-import csv
 import argparse
 import time
-import os.path
-from os import path
 from CRDP_REST_API import *
 import random
 from tqdm import tqdm
 from termcolor import colored
 
+
 #  --------- DEFINED Routines -----------------
 def getRNDStr(t_len):
     # Simple routine that generates a randon plaintext payload
 
-    return ''.join(random.choices("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=t_len))
+    return "".join(
+        random.choices(
+            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=t_len
+        )
+    )
 #
 
 #####################################################################
 # Code for collecting and parsing input information from command line
 #####################################################################
 parser = argparse.ArgumentParser()
-parser.add_argument('-e', nargs=1, action='store', required=True, dest='hostnameCRDP')
-parser.add_argument('-p', nargs=1, action='store', required=True, dest='protectionPolicy')
-parser.add_argument('-b', nargs=1, action='store', required=True, dest='batchSize', type=int)
-parser.add_argument('-u', nargs=1, action='store', required=True, dest='username')
-parser.add_argument('-bulk', action=argparse.BooleanOptionalAction)
+parser.add_argument("-e", nargs=1, action="store", required=True, dest="hostnameCRDP")
+parser.add_argument(
+    "-p", nargs=1, action="store", required=True, dest="protectionPolicy"
+)
+parser.add_argument(
+    "-b", nargs=1, action="store", required=True, dest="batchSize", type=int
+)
+parser.add_argument("-u", nargs=1, action="store", required=True, dest="username")
+parser.add_argument("-bulk", action=argparse.BooleanOptionalAction)
 
 args = parser.parse_args()
 
 
 tmpStr = "\nCDRP Stress starting... LET THE STRESS BEGIN!"
-print(colored(tmpStr, 'white', attrs=["underline"]))
+print(colored(tmpStr, "white", attrs=["underline"]))
 
 # Echo Input Parameters
-hostCRDP            = args.hostnameCRDP[0]
-batchSize           = args.batchSize[0]
-protectionPolicy    = args.protectionPolicy[0]
-bulkFlag            = False
+hostCRDP = args.hostnameCRDP[0]
+batchSize = args.batchSize[0]
+protectionPolicy = args.protectionPolicy[0]
+bulkFlag = False
 if args.bulk:
     bulkFlag = True
-r_user              = args.username[0]
+r_user = args.username[0]
 
 #####################################################################
 # Echo back input information for validation
 #####################################################################
 print(" Input Parameters:")
-tmpStr = "  CRDPHost: %s\n  BatchSize: %s\n  ProtectionPolicy: %s\n  BulkProtection: %s\n" %(hostCRDP, batchSize, protectionPolicy, bulkFlag)
+tmpStr = (
+    "  CRDPHost: %s\n  BatchSize: %s\n  ProtectionPolicy: %s\n  BulkProtection: %s\n"
+    % (hostCRDP, batchSize, protectionPolicy, bulkFlag)
+)
 print(tmpStr)
 
 # Get a string of 64 random characters.
-c_data          = getRNDStr(48)
+c_data = getRNDStr(48)
 
 # Reserve some variables for later use
-c_data_array    = [] #reserve for later use
-p_data          = [] #reserve for later use
-p_data_array    = [] #reserve for later use
-p_version       = [] #reserve for later use
-r_data          = [] #reserve for later use
-r_data_array    = [] #reserve for later use
+c_data_array = []  # reserve for later use
+p_data = []  # reserve for later use
+p_data_array = []  # reserve for later use
+p_version = []  # reserve for later use
+r_data = []  # reserve for later use
+r_data_array = []  # reserve for later use
 
 # how many times do we want to encrypt it?
 c_count = batchSize
 
 #####################################################################
-# Let's encrypt the data as fast as we can in two ways:  
+# Let's encrypt the data as fast as we can in two ways:
 # 1) as individual records or 2) as bulk data (faster)
 #####################################################################
 starttime = time.time()
 
-print(colored("*** CRDP PROTECTION Test Started ***", 'white', attrs=["bold"]))
+print(colored("*** CRDP PROTECTION Test Started ***", "white", attrs=["bold"]))
 print(" --> Start time: ", starttime)
 
 if bulkFlag == False:
     # time - re-retrieve start time
     starttime = time.time()
-    
+
     for i in tqdm(range(c_count), desc="Discrete PROTECT Progress"):
         p_data, p_version = protectData(hostCRDP, c_data, protectionPolicy)
 
@@ -101,33 +109,38 @@ else:
     # time - re-retrieve start time
     starttime = time.time()
     p_data_array, p_version = protectBulkData(hostCRDP, c_data_array, protectionPolicy)
-    p_data = p_data_array[0][CRDP_PROTECTED_DATA_NAME]  # retreive first recorded in returned data
+    p_data = p_data_array[0][
+        CRDP_PROTECTED_DATA_NAME
+    ]  # retreive first recorded in returned data
 
 
 # time - get end time
 endtime = time.time()
 print(" -->   End time: ", endtime)
 
-deltatimesec = (endtime-starttime)
-pRate = c_count/deltatimesec
+deltatimesec = endtime - starttime
+pRate = c_count / deltatimesec
 
-outStr = "\n* CRDP Test Completed - PROTECT. %s plaintext strings processed. Process time: %5.2f sec.  Rate: %5.2f tps.\n" %(c_count, deltatimesec, pRate) 
-print(colored(outStr, 'light_green', attrs=["bold"]))
+outStr = (
+    "\nCRDP Test Completed - PROTECT. %s plaintext strings processed. Process time: %5.2f sec.  Rate: %5.2f tps.\n"
+    % (c_count, deltatimesec, pRate)
+)
+print(colored(outStr, "light_green", attrs=["bold"]))
 
 
 #####################################################################
-# Let's decrypt the data as fast as we can in two ways:  
+# Let's decrypt the data as fast as we can in two ways:
 # 1) as individual records or 2) as bulk data (faster)
 #####################################################################
 starttime = time.time()
 
-print(colored("*** CRDP REVEAL Test Started ***", 'white', attrs=["bold"]))
+print(colored("*** CRDP REVEAL Test Started ***", "white", attrs=["bold"]))
 print(" --> Start time: ", starttime)
 
 if bulkFlag == False:
     # time - re-retrieve start time
     starttime = time.time()
-    
+
     for i in tqdm(range(c_count), desc="Discrete REVEAL Progress"):
         r_data = revealData(hostCRDP, p_data, protectionPolicy, p_version, r_user)
 
@@ -139,22 +152,24 @@ else:
 
     # time - re-retrieve start time
     starttime = time.time()
-    r_data_array = revealBulkData(hostCRDP, p_data_array, protectionPolicy, p_version, r_user)
+    r_data_array = revealBulkData(
+        hostCRDP, p_data_array, protectionPolicy, p_version, r_user
+    )
     r_data = r_data_array[0][CRDP_DATA_NAME]  # retreive first recorded in returned data
-    
+
 
 # time - get end time
 endtime = time.time()
 print(" -->   End time: ", endtime)
 
-deltatimesec = (endtime-starttime)
-pRate = c_count/deltatimesec
+deltatimesec = endtime - starttime
+pRate = c_count / deltatimesec
 
-outStr = "\n* CRDP Test Completed - REVEAL. %s ciphertrext strings processed. Process time: %5.2f sec.  Rate: %5.2f tps.\n" %(c_count, deltatimesec, pRate) 
-print(colored(outStr, 'light_green', attrs=["bold"]))
+outStr = (
+    "\nCRDP Test Completed - REVEAL. %s ciphertrext strings processed. Process time: %5.2f sec.  Rate: %5.2f tps.\n"
+    % (c_count, deltatimesec, pRate)
+)
+print(colored(outStr, "light_green", attrs=["bold"]))
 
-outStr = "    PT: %s\n    CT: %s\n    RT: %s" %(c_data, p_data, r_data) 
+outStr = "    PT: %s\n    CT: %s\n    RT: %s" % (c_data, p_data, r_data)
 print(outStr)
-
-
-
